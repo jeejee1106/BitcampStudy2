5964f4a81d8fa30818e6a26ab4e99b05e7d664fb
minjee <kimmj1106@naver.com>
2021-08-21 02:36:52 +0900

    0821이클립스8버전 삭제 후 재설치할거임
    8버전에서의 마지막(?) 푸시!

--- src/day0820/Ex01SawonJDBC.java
+++ src/day0820/Ex01SawonJDBC.java
@@ -8,61 +8,62 @@ import java.sql.Statement;
 
 public class Ex01SawonJDBC {
 	static final String ORACLEDRIVER = "oracle.jdbc.driver.OracleDriver";
-	static final String ORACLE_CLOUD = "jdbc:oracle:thin:@bitcamp_high?TNS_ADMIN=D:/bitjava0719/OracleCloud"; //오라클 클라우드 쓸때!!!!!!!!!!!!!!!!!!!
+//	static final String ORACLE_CLOUD = "jdbc:oracle:thin:@bitcamp_high?TNS_ADMIN=D:/bitjava0719/OracleCloud"; //오라클 클라우드 쓸때!!!!!!!!!!!!!!!!!!!
+	static final String ORACLE_CLOUD = "jdbc:oracle:thin:@bitcamp_high?TNS_ADMIN=C:/Users/PC/Desktop/study/OracleCloud"; //오라클 클라우드 쓸때!!!!!!!!!!!!!!!!!!!
 	static final String OLACLE_LOCAL = "jdbc:oracle:thin:@localhost:1521:xe"; //로컬로 오라클 연결할 때!!!!!!!!!!	
 	
 	
 	
 	//드라이버는 한번만 실행하므로 주로 생성자에서 코드를 추가한다.
-	public Ex01SawonJDBC() {
-		//문자열인 클래스를 클래스인스턴스로 변환
-		try {
-			Class.forName(ORACLEDRIVER);
-			System.out.println("오라클 드라이버 성공");
-		} catch (ClassNotFoundException e) {
-			System.out.println("오라클 드라이버 실패:" + e.getMessage()); //클래스가 들어있는 자료파일이 없는 것
-		}
-	}
+//	public Ex01SawonJDBC() {
+//		//문자열인 클래스를 클래스인스턴스로 변환
+//		try {
+//			Class.forName(ORACLEDRIVER);
+//			System.out.println("오라클 드라이버 성공");
+//		} catch (ClassNotFoundException e) {
+//			System.out.println("오라클 드라이버 실패:" + e.getMessage()); //클래스가 들어있는 자료파일이 없는 것
+//		}
+//	}
 	
 	//오라클 클라우드에 19c 추가한 사람들은 이 메서드 추가
-	public void connectCloudSawon() { //강사님 아이디랑 비번 빌린거임!
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null; //select sql문은 ResultSet을 얻어야 데이터를 가져올 수 있다.
-		String sql = "select * from sawon order by num";
-		
-		try {
-			conn = DriverManager.getConnection(ORACLE_CLOUD, "admin", "Moon2549466**");
-			System.out.println("클라우드의 오라클 서버에 연결 성공");
-			stmt=conn.createStatement();
-			rs = stmt.executeQuery(sql);
-			//여러줄을 가져와야 할 경우 while문으로 가져온다.
-			//rs.next() : 다음 데이터로 이동하면서 true반환, 더 이상 데이터가 없으면 false반환
-			while(rs.next()) {
-				//DB로부터 데이터 가져오기
-				int num = rs.getInt("num");
-				String name = rs.getString("name");
-				String gender = rs.getString("gender");
-				String buseo = rs.getString("buseo");
-				int pay = rs.getInt("pay");
-				System.out.println(num + "\t" + name + "\t" + gender + "\t" + buseo + "\t" + pay);
-				
-			}
-			
-			
-		} catch (SQLException e) {
-			System.out.println("실패 : " + e.getMessage());
-			
-		} finally {
-			try {
-				if(rs!=null) rs.close();
-				if(stmt!=null) stmt.close();
-				if(conn!=null) conn.close();
-			} catch(SQLException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+//	public void connectCloudSawon() { //강사님 아이디랑 비번 빌린거임!
+//		Connection conn = null;
+//		Statement stmt = null;
+//		ResultSet rs = null; //select sql문은 ResultSet을 얻어야 데이터를 가져올 수 있다.
+//		String sql = "select * from sawon order by num";
+//		
+//		try {
+//			conn = DriverManager.getConnection(ORACLE_CLOUD, "admin", "Moon2549466**");
+//			System.out.println("클라우드의 오라클 서버에 연결 성공");
+//			stmt=conn.createStatement();
+//			rs = stmt.executeQuery(sql);
+//			//여러줄을 가져와야 할 경우 while문으로 가져온다.
+//			//rs.next() : 다음 데이터로 이동하면서 true반환, 더 이상 데이터가 없으면 false반환
+//			while(rs.next()) {
+//				//DB로부터 데이터 가져오기
+//				int num = rs.getInt("num");
+//				String name = rs.getString("name");
+//				String gender = rs.getString("gender");
+//				String buseo = rs.getString("buseo");
+//				int pay = rs.getInt("pay");
+//				System.out.println(num + "\t" + name + "\t" + gender + "\t" + buseo + "\t" + pay);
+//				
+//			}
+//			
+//			
+//		} catch (SQLException e) {
+//			System.out.println("실패 : " + e.getMessage());
+//			
+//		} finally {
+//			try {
+//				if(rs!=null) rs.close();
+//				if(stmt!=null) stmt.close();
+//				if(conn!=null) conn.close();
+//			} catch(SQLException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//	}
 	
 	
 	//자기 컴퓨터 로컬의 오라클 연결하려는 메서드!!!!!!!!!!
@@ -108,7 +109,7 @@ public class Ex01SawonJDBC {
 	
 	public static void main(String[] args) {
 		Ex01SawonJDBC ex = new Ex01SawonJDBC();
-		ex.connectCloudSawon();
+//		ex.connectCloudSawon();
 		System.out.println("==============================================");
 		ex.connectLocalFood();
 	}
